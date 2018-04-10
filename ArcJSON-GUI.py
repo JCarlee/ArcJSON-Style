6c7561cee1b5c7ca54ceebe9c879d0f914fb2855
jcarlee <jcarlee@gmail.com>
2018-04-10 17:56:17 -0500

    First tab cleanup

--- ArcJSON-GUI.py
+++ ArcJSON-GUI.py
@@ -7,21 +7,18 @@ window = Tk()
 
 window.title("ArcJSON")  # Cosmetic window title
 
+simple_point_names = ['Style', 'Size', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-T', 'Line-R', 'Line-G',
+                      'Line-B', 'Line-T', 'OL-Width']
 
-def unique_point():
+simple_line_names = ['Style', 'Width', 'Line-R', 'Line-G', 'Line-B', 'Line-T']
+
+simple_polygon_names = ['Fill Style', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-T', 'Line Style', 'Line-R', 'Line-G', 'Line-B',
+                        'Line-T']
+
+
+def simple_point():
     tab1.filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("txt", "*.txt"), ("all files",
                                                                                                         "*.*")))
-    fill_r = s3.get()
-    fill_g = s4.get()
-    fill_b = s5.get()
-    fill_t = s6.get()
-    size = s2.get()
-    style = s1.get()
-    outline_r = s7.get()
-    outline_g = s8.get()
-    outline_b = s9.get()
-    outline_t = s10.get()
-    outline_w = s11.get()
     tab1_file = open(tab1.filename, 'w')
     tab1_file.write('''{{
     "renderer": {{
@@ -52,8 +49,36 @@ def unique_point():
                     }}
             }}
     }}
-}}'''.format(fill_r, fill_g, fill_b, fill_t, size, style, outline_r, outline_g, outline_b, outline_t, outline_w))
+}}'''.format(s3.get(), s4.get(), s5.get(), s6.get(), s2.get(), s1.get(), s7.get(), s8.get(), s9.get(), s10.get(),
+             s11.get()))
     tab1_file.close()
+
+
+def simple_line():
+    tab2.filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("txt", "*.txt"), ("all files",
+                                                                                                        "*.*")))
+    tab2_file = open(tab2.filename, 'w')
+    tab2_file.write('''{{
+  "renderer": {{
+    "type": "simple",
+    "symbol": {{
+      "type": "esriSLS",
+      "color": [
+        {0},
+        {1},
+        {2},
+        {3}
+      ],
+      "width": {4},
+      "style": "{5}"
+    }}
+  }}
+}}'''.format(u3.get(), u4.get(), u5.get(), u6.get(), u2.get(), u1.get(), ))
+    tab2_file.close()
+
+
+def simple_polygon():
+    pass
 
 
 # =====Create Tab Structure=====
@@ -65,14 +90,12 @@ tab2 = ttk.Frame(tab_control)
 tab3 = ttk.Frame(tab_control)
 
 # Add defined tabs to window
-tab_control.add(tab1, text='Simple')
-tab_control.add(tab2, text='Unique')
-tab_control.add(tab3, text='Class Break')
+tab_control.add(tab1, text='Point')
+tab_control.add(tab2, text='Line')
+tab_control.add(tab3, text='Polygon')
 
 
-# ===========Simple=============
-simple_point_names = ['Style', 'Size', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-Transp', 'Outline-R', 'Outline-G',
-                      'Outline-B', 'OL-Transp', 'OL-Width']
+# ===========Point=============
 for i in simple_point_names:
     Label(tab1, text=i).grid(row=simple_point_names.index(i))
 
@@ -100,19 +123,11 @@ s9.grid(row=8, column=1, sticky=W + E)
 s10.grid(row=9, column=1, sticky=W + E)
 s11.grid(row=10, column=1, sticky=W + E)
 
-Button(tab1, text='Save', command=unique_point).grid(row=11, column=1, sticky=W, pady=4)
+Button(tab1, text='Save', command=simple_point).grid(row=11, column=1, sticky=W, pady=4)
 
-# ===========Unique===========
-Label(tab2, text="Style").grid(row=0)
-Label(tab2, text="Size").grid(row=1)
-Label(tab2, text="Fill-R").grid(row=2)
-Label(tab2, text="Fill-G").grid(row=3)
-Label(tab2, text="Fill-B").grid(row=4)
-Label(tab2, text="Fill-Transp").grid(row=5)
-Label(tab2, text="Outline-R").grid(row=6)
-Label(tab2, text="Outline-G").grid(row=7)
-Label(tab2, text="Outline-B").grid(row=8)
-Label(tab2, text="OL-Transp").grid(row=9)
+# ===========Line===========
+for i in simple_line_names:
+    Label(tab2, text=i).grid(row=simple_line_names.index(i))
 
 u1 = Entry(tab2)
 u2 = Entry(tab2)
@@ -120,10 +135,6 @@ u3 = Entry(tab2)
 u4 = Entry(tab2)
 u5 = Entry(tab2)
 u6 = Entry(tab2)
-u7 = Entry(tab2)
-u8 = Entry(tab2)
-u9 = Entry(tab2)
-u10 = Entry(tab2)
 
 u1.grid(row=0, column=1, sticky=W + E)
 u2.grid(row=1, column=1, sticky=W + E)
@@ -131,10 +142,36 @@ u3.grid(row=2, column=1, sticky=W + E)
 u4.grid(row=3, column=1, sticky=W + E)
 u5.grid(row=4, column=1, sticky=W + E)
 u6.grid(row=5, column=1, sticky=W + E)
-u7.grid(row=6, column=1, sticky=W + E)
-u8.grid(row=7, column=1, sticky=W + E)
-u9.grid(row=8, column=1, sticky=W + E)
-u10.grid(row=9, column=1, sticky=W + E)
+
+Button(tab2, text='Save', command=simple_line).grid(row=6, column=1, sticky=W, pady=4)
+
+# ===========Polygon===========
+for i in simple_polygon_names:
+    Label(tab3, text=i).grid(row=simple_polygon_names.index(i))
+
+p1 = Entry(tab3)
+p2 = Entry(tab3)
+p3 = Entry(tab3)
+p4 = Entry(tab3)
+p5 = Entry(tab3)
+p6 = Entry(tab3)
+p7 = Entry(tab3)
+p8 = Entry(tab3)
+p9 = Entry(tab3)
+p10 = Entry(tab3)
+
+p1.grid(row=0, column=1, sticky=W + E)
+p2.grid(row=1, column=1, sticky=W + E)
+p3.grid(row=2, column=1, sticky=W + E)
+p4.grid(row=3, column=1, sticky=W + E)
+p5.grid(row=4, column=1, sticky=W + E)
+p6.grid(row=5, column=1, sticky=W + E)
+p7.grid(row=6, column=1, sticky=W + E)
+p8.grid(row=7, column=1, sticky=W + E)
+p9.grid(row=8, column=1, sticky=W + E)
+p10.grid(row=9, column=1, sticky=W + E)
+
+Button(tab3, text='Save', command=simple_polygon).grid(row=20, column=1, sticky=W, pady=4)
 
 tab_control.pack(expand=1, fill='both')
 
