7929ccf98d82981981bedf0990ec81294edcd90a
jcarlee <jcarlee@gmail.com>
2018-04-11 18:28:44 -0500

    First implementation of drop-down using ComboBox

--- ArcJSON-GUI.py
+++ ArcJSON-GUI.py
@@ -12,8 +12,8 @@ simple_point_names = ['Style', 'Size', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-T', '
 
 simple_line_names = ['Style', 'Width', 'Line-R', 'Line-G', 'Line-B', 'Line-T']
 
-simple_polygon_names = ['Fill Style', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-T', 'Line Style', 'Line-R', 'Line-G', 'Line-B',
-                        'Line-T']
+simple_polygon_names = ['Fill Style', 'Fill-R', 'Fill-G', 'Fill-B', 'Fill-T', 'Line Style', 'Line Width', 'Line-R',
+                        'Line-G', 'Line-B', 'Line-T']
 
 
 def simple_point():
@@ -49,8 +49,8 @@ def simple_point():
                     }}
             }}
     }}
-}}'''.format(s3.get(), s4.get(), s5.get(), s6.get(), s2.get(), s1.get(), s7.get(), s8.get(), s9.get(), s10.get(),
-             s11.get()))
+}}'''.format(point3.get(), point4.get(), point5.get(), point6.get(), point2.get(), point1.get(), point7.get(),
+             point8.get(), point9.get(), point10.get(), point11.get()))
     tab1_file.close()
 
 
@@ -73,12 +73,42 @@ def simple_line():
       "style": "{5}"
     }}
   }}
-}}'''.format(u3.get(), u4.get(), u5.get(), u6.get(), u2.get(), u1.get(), ))
+}}'''.format(line3.get(), line4.get(), line5.get(), line6.get(), line2.get(), line1.get()))
     tab2_file.close()
 
 
 def simple_polygon():
-    pass
+    tab3.filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("txt", "*.txt"), ("all files",
+                                                                                                        "*.*")))
+    tab3_file = open(tab3.filename, 'w')
+    tab3_file.write('''{{
+  "renderer": {{
+    "type": "simple",
+    "symbol": {{
+      "type": "esriSFS",
+      "color": [
+        {0},
+        {1},
+        {2},
+        {3}
+      ],
+      "outline": {{
+        "type": "esriSLS",
+        "color": [
+          {4},
+          {5},
+          {6},
+          {7}
+        ],
+        "width": {8},
+        "style": "{9}"
+      }},
+      "style": "{10}"
+    }}
+  }}
+}}'''.format(poly2.get(), poly3.get(), poly4.get(), poly5.get(), poly8.get(), poly9.get(), poly10.get(), poly11.get(),
+             poly7.get(), poly6.get(), poly1.get()))
+    tab3_file.close()
 
 
 # =====Create Tab Structure=====
@@ -99,29 +129,26 @@ tab_control.add(tab3, text='Polygon')
 for i in simple_point_names:
     Label(tab1, text=i).grid(row=simple_point_names.index(i))
 
-s1 = Entry(tab1)
-s2 = Entry(tab1)
-s3 = Entry(tab1)
-s4 = Entry(tab1)
-s5 = Entry(tab1)
-s6 = Entry(tab1)
-s7 = Entry(tab1)
-s8 = Entry(tab1)
-s9 = Entry(tab1)
-s10 = Entry(tab1)
-s11 = Entry(tab1)
+point1_options = ["esriSMSCircle", "esriSMSCross", "esriSMSDiamond", "esriSMSSquare", "esriSMSX"]
 
-s1.grid(row=0, column=1, sticky=W + E)
-s2.grid(row=1, column=1, sticky=W + E)
-s3.grid(row=2, column=1, sticky=W + E)
-s4.grid(row=3, column=1, sticky=W + E)
-s5.grid(row=4, column=1, sticky=W + E)
-s6.grid(row=5, column=1, sticky=W + E)
-s7.grid(row=6, column=1, sticky=W + E)
-s8.grid(row=7, column=1, sticky=W + E)
-s9.grid(row=8, column=1, sticky=W + E)
-s10.grid(row=9, column=1, sticky=W + E)
-s11.grid(row=10, column=1, sticky=W + E)
+# point1 = Entry(tab1)  # Style
+point1 = ttk.Combobox(tab1, values=point1_options)
+point1.set("esriSMSCircle")
+point2 = Entry(tab1)  # Size
+point3 = Entry(tab1)  # Fill-R
+point4 = Entry(tab1)  # Fill-G
+point5 = Entry(tab1)  # Fill-B
+point6 = Entry(tab1)  # Fill-T
+point7 = Entry(tab1)  # Line-R
+point8 = Entry(tab1)  # Line-G
+point9 = Entry(tab1)  # Line-B
+point10 = Entry(tab1)  # Line-T
+point11 = Entry(tab1)  # OL-Width
+
+point_list = [point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11]
+
+for i in point_list:
+    i.grid(row=point_list.index(i), column=1, sticky=W + E)
 
 Button(tab1, text='Save', command=simple_point).grid(row=11, column=1, sticky=W, pady=4)
 
@@ -129,19 +156,17 @@ Button(tab1, text='Save', command=simple_point).grid(row=11, column=1, sticky=W,
 for i in simple_line_names:
     Label(tab2, text=i).grid(row=simple_line_names.index(i))
 
-u1 = Entry(tab2)
-u2 = Entry(tab2)
-u3 = Entry(tab2)
-u4 = Entry(tab2)
-u5 = Entry(tab2)
-u6 = Entry(tab2)
+line1 = Entry(tab2)
+line2 = Entry(tab2)
+line3 = Entry(tab2)
+line4 = Entry(tab2)
+line5 = Entry(tab2)
+line6 = Entry(tab2)
 
-u1.grid(row=0, column=1, sticky=W + E)
-u2.grid(row=1, column=1, sticky=W + E)
-u3.grid(row=2, column=1, sticky=W + E)
-u4.grid(row=3, column=1, sticky=W + E)
-u5.grid(row=4, column=1, sticky=W + E)
-u6.grid(row=5, column=1, sticky=W + E)
+line_list = [line1, line2, line3, line4, line5, line6]
+
+for i in line_list:
+    i.grid(row=line_list.index(i), column=1, sticky=W + E)
 
 Button(tab2, text='Save', command=simple_line).grid(row=6, column=1, sticky=W, pady=4)
 
@@ -149,29 +174,24 @@ Button(tab2, text='Save', command=simple_line).grid(row=6, column=1, sticky=W, p
 for i in simple_polygon_names:
     Label(tab3, text=i).grid(row=simple_polygon_names.index(i))
 
-p1 = Entry(tab3)
-p2 = Entry(tab3)
-p3 = Entry(tab3)
-p4 = Entry(tab3)
-p5 = Entry(tab3)
-p6 = Entry(tab3)
-p7 = Entry(tab3)
-p8 = Entry(tab3)
-p9 = Entry(tab3)
-p10 = Entry(tab3)
+poly1 = Entry(tab3)
+poly2 = Entry(tab3)
+poly3 = Entry(tab3)
+poly4 = Entry(tab3)
+poly5 = Entry(tab3)
+poly6 = Entry(tab3)
+poly7 = Entry(tab3)
+poly8 = Entry(tab3)
+poly9 = Entry(tab3)
+poly10 = Entry(tab3)
+poly11 = Entry(tab3)
 
-p1.grid(row=0, column=1, sticky=W + E)
-p2.grid(row=1, column=1, sticky=W + E)
-p3.grid(row=2, column=1, sticky=W + E)
-p4.grid(row=3, column=1, sticky=W + E)
-p5.grid(row=4, column=1, sticky=W + E)
-p6.grid(row=5, column=1, sticky=W + E)
-p7.grid(row=6, column=1, sticky=W + E)
-p8.grid(row=7, column=1, sticky=W + E)
-p9.grid(row=8, column=1, sticky=W + E)
-p10.grid(row=9, column=1, sticky=W + E)
+poly_list = [poly1, poly2, poly3, poly4, poly5, poly6, poly7, poly8, poly9, poly10, poly11]
 
-Button(tab3, text='Save', command=simple_polygon).grid(row=20, column=1, sticky=W, pady=4)
+for i in poly_list:
+    i.grid(row=poly_list.index(i), column=1, sticky=W + E)
+
+Button(tab3, text='Save', command=simple_polygon).grid(row=10, column=1, sticky=W, pady=4)
 
 tab_control.pack(expand=1, fill='both')
 
